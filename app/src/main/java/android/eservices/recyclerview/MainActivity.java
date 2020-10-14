a2cef4a34be0aa00adc3dacfcda7b7f01d3ebfb3
Fliro59 antoine <antoine.meresse@orange.fr>
2020-10-14 18:05:44 +0200

    Switch layout possible

--- app/src/main/java/android/eservices/recyclerview/MainActivity.java
+++ app/src/main/java/android/eservices/recyclerview/MainActivity.java
@@ -3,6 +3,7 @@ package android.eservices.recyclerview;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -11,6 +12,7 @@ import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity implements GameActionInterfa
     private RecyclerView.LayoutManager layoutManager;
     private Toolbar toolbar;
     private CoordinatorLayout coordinatorLayout;
+    private FloatingActionButton changeLayoutButton;
+    private boolean switchBoolean;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +32,11 @@ public class MainActivity extends AppCompatActivity implements GameActionInterfa
         setContentView(R.layout.activity_main);
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        changeLayoutButton = findViewById(R.id.fabSwitch);
 
         coordinatorLayout = findViewById(R.id.coordinator_layout);
         setupRecyclerView();
+
     }
 
     private void setupRecyclerView() {
@@ -39,11 +45,29 @@ public class MainActivity extends AppCompatActivity implements GameActionInterfa
         recyclerView = findViewById(R.id.my_recyclerview);
         recyclerView.setHasFixedSize(true);
 
-        // Layout :
-        //layoutManager = new LinearLayoutManager(this);
-        layoutManager = new GridLayoutManager(this,2);
-        // Assign layout
+        final RecyclerView.LayoutManager layoutManager_lign = new LinearLayoutManager(this);
+        final RecyclerView.LayoutManager layoutManager_grid = new GridLayoutManager(this,2);
+
+        layoutManager = layoutManager_lign;
         recyclerView.setLayoutManager(layoutManager);
+
+        changeLayoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(switchBoolean) {
+                    switchBoolean = false;
+                    layoutManager = layoutManager_lign;
+                }
+                else {
+                    switchBoolean = true;
+                    layoutManager = layoutManager_grid;
+                }
+                recyclerView.setLayoutManager(layoutManager);
+            }
+        });
+
+        // Assign layout
+        //recyclerView.setLayoutManager(layoutManager);
 
         //Use data generator to get data to display.
         List<GameViewModel> myDataset = DataGenerator.generateData();
